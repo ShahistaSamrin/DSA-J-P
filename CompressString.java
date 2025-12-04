4e8d0814b39f51393f7b5c01eb8fb206f5fcca6d
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2025-12-04 14:55:49 +0530

    Add files via upload

---
+++ CompressString.java
@@ -0,0 +1,20 @@
+ //OUTPUT: a2b3c3d5
+public class CompressString {
+    public static void main(String[] args) {
+        String str = "aabbbcccddddd";
+        String res="";
+        for(int i=0;i<str.length();i++){
+            char s=str.charAt(i);
+            if(res.contains(String.valueOf(s)))
+                continue;
+            int count =0;
+            for(int j=0;j<str.length();j++){
+                if(str.charAt(j)==s){
+                    count++;
+                }
+            }
+            res=res+s+count;
+        }
+        System.out.println(res);
+    }
+}
