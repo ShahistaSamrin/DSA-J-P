68b1db885fa54e436185a7cc3c366dd52029a86d
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2025-12-04 15:43:10 +0530

    Add files via upload

---
+++ RemoveDuplicatesInString.java
@@ -0,0 +1,18 @@
+//OUTPUT:misisipi
+public class RemoveDuplicatesInString {
+   public static void main(String[] args) {
+
+        String str = "mississippi";
+        String result = "";
+
+        result += str.charAt(0); // first character
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(i - 1)) {
+                result += str.charAt(i);
+            }
+        }
+
+        System.out.println(result);
+    } 
+}
